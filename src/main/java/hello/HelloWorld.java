1c21d96b0717b564ff65ba97bd17a4f4c1dc6011
Jacob Blazusiak <jblazusi@ualberta.ca>
2020-10-08 21:58:46 -0600

    Init

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HelloWorld {
+  public static int main(String[] args) {
+	Greeter greeter = new Greeter();
+	System.out.println(greeter.sayHello());
+        //String s = null;
+	//return s.length();
+  }
+}
